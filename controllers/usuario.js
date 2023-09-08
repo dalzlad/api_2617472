4e00559f4468cd42eefe1a35ae189f1958e68765
dalzlad <dalz.sena@gmail.com>
2023-09-08 07:30:06 -0500

    cambios post

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -34,7 +34,7 @@ const usuarioPost = async(req, res) => {
 //Modifcación
 const usuarioPut = async(req, res = response) => {
 
-    const {nombre, password, rol, estado} = req.query
+    const {nombre, password, rol, estado} = req.body
     let mensaje = 'Modificación exitosa'
     try{
          await Usuario.findOneAndUpdate({nombre: nombre}, 
