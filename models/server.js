342cae451e3b779b2551ed7db6fdaf879610b3d4
dalzlad <dalz.sena@gmail.com>
2023-09-08 07:19:54 -0500

    middlewares

--- models/server.js
+++ models/server.js
@@ -4,12 +4,19 @@ const cors  = require('cors');//Implementar seguridad
 const bodyParser = require('body-parser')//Recibir datos de formularios html
 const { dbConnection } = require('../database/config')
 
+const cookieParser = require('cookie-parser');
+const express = require('express')
+const { dbConnection } = require('../database/config')
+const cors  = require('cors');//Implementar seguridad
+const bodyParser = require('body-parser')//Recibir datos de formularios html
+
 class Server{
 
     constructor(){
         this.app = express()
         this.port = process.env.PORT //Capturando variable puerto
         this.usuarioPath = '/api/usuario' //Ruta pública
+        this.middlewares()
         this.conectarDB()
         this.routes()
     }
@@ -18,6 +25,13 @@ class Server{
         this.app.listen(this.port, () => {
             console.log(`Escuchando por el puerto ${this.port}`)
         })
+    }
+
+    middlewares(){
+        this.app.use(cookieParser()); 
+        this.app.use(express.static(__dirname + "/public"));
+        this.app.use( cors() );
+        this.app.use(bodyParser.json()) // for parsing application/json
     }
 
     routes(){
